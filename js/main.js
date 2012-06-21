ade488b2ebac8e4244ee2cde31d298f948e93c52
Alain Horner <alain.horner@liip.ch>
2012-06-21 22:53:26 +0200

    Added Lena

--- js/main.js
+++ js/main.js
@@ -8,6 +8,16 @@ YUI({
 
     'use strict';
 
-    Y.log("hello stego world");
+    window.onload = function() {
+        var canvas = document.getElementById('drawing_area'),
+            context = canvas.getContext("2d"),
+            myImg = new Image();
+
+        myImg.onload = function() {
+          context.drawImage(myImg, 0, 0, 300, 300);
+        };
+
+        myImg.src = "img/lena.bmp";
+    };
 
 });
