8db735cdafc351db08ee45ea1a361476d9dc4f28
Alain Horner <alain.horner@liip.ch>
2012-06-23 14:36:28 +0200

    Basic hiding of images

---
+++ js/combiner.js
@@ -0,0 +1,59 @@
+YUI.add('combiner', function (Y) {
+
+    'use strict';
+
+    function Combiner(config) {
+        Combiner.superclass.constructor.apply(this, arguments);
+    }
+
+    Combiner.NAME = "combiner";
+
+    Combiner.ATTRS = {
+    }
+
+    // TODO: make amount of used bits for hiding variable
+
+    Y.extend(Combiner, Y.Base, {
+
+        // maps an 8-bit value to a 1-bit value
+        mapToOneBit: function (oriVal) {
+            return (oriVal >= 128) ? 1 : 0;
+        },
+
+        // minifies an image so each pixel only needs 4 bit
+        minify: function (sourceData, multiplier) {
+            var originalData = sourceData.data,
+                n = originalData.length,
+                i;
+
+            // loop through original data and minify it
+            for (i = 0; i < n; i += 4) {
+                originalData[i  ] = this.mapToOneBit(originalData[i  ]) * multiplier;// red
+                originalData[i+1] = this.mapToOneBit(originalData[i+1]) * multiplier;// green
+                originalData[i+2] = this.mapToOneBit(originalData[i+2]) * multiplier;// blue
+            }
+
+            return sourceData;
+        },
+
+        // combines two images by hiding the second in the first
+        combine: function (containerData, sourceData) {
+            var containerPixels = containerData.data,
+                minifiedPixels = this.minify(sourceData, 1).data,
+                n = minifiedPixels.length,
+                i;
+
+            for (i = 0; i < 255; i += 4) {
+                containerPixels[i  ] = ((containerPixels[i  ] >> 1) << 1) | minifiedPixels[i  ];
+                containerPixels[i+1] = ((containerPixels[i+1] >> 1) << 1) | minifiedPixels[i+1];
+                containerPixels[i+2] = ((containerPixels[i+2] >> 1) << 1) | minifiedPixels[i+2];
+            }
+
+            return containerData;
+        }
+
+    });
+
+    Y.Combiner = Combiner;
+
+}, '0.1', {requires: ['base']});
--- js/main.js
+++ js/main.js
@@ -1,23 +1,46 @@
 YUI({
     modules: {
-        'dummy': {
-            fullpath: "js/dummy.js"
+        'combiner': {
+            fullpath: "js/combiner.js"
         }
     }
-}).use('dummy', function (Y) {
+}).use('node', 'combiner', function (Y) {
 
     'use strict';
 
     window.onload = function() {
-        var canvas = document.getElementById('drawing_area'),
-            context = canvas.getContext("2d"),
-            myImg = new Image();
+        var containerContext = Y.one('#container canvas').invoke('getContext', '2d'),
+            originalContext = Y.one('#original canvas').invoke('getContext', '2d'),
+            hiddenContext = Y.one('#hidden canvas').invoke('getContext', '2d'),
+            resultContext = Y.one('#result canvas').invoke('getContext', '2d'),
+            containerImg = new Image(),
+            originalImg = new Image(),
+            combiner = new Y.Combiner();
 
-        myImg.onload = function() {
-          context.drawImage(myImg, 0, 0, 300, 300);
+        // draw original image
+        containerImg.onload = function() {
+          containerContext.drawImage(containerImg, 0, 0, 300, 300);
         };
 
-        myImg.src = "img/lena.bmp";
+        originalImg.onload = function() {
+          originalContext.drawImage(originalImg, 0, 0, 300, 300);
+        };
+
+        containerImg.src = "img/lena.png";
+        originalImg.src = "img/fribourg.png";
+
+        // minify original image
+        setTimeout(function () {
+            var minifiedImageData = combiner.minify(originalContext.getImageData(0, 0, 300, 300), 255);// TODO: could we directly pass it the image?
+            hiddenContext.putImageData(minifiedImageData, 0, 0);
+        }, 100);// TODO: improve, so that we somehow get an event
+
+        // hide original image in container image
+        setTimeout(function () {
+            var combinedImageData = combiner.combine(containerContext.getImageData(0, 0, 300, 300), originalContext.getImageData(0, 0, 300, 300));
+            resultContext.putImageData(combinedImageData, 0, 0);
+        }, 100);// TODO: improve, so that we somehow get an event
+
     };
 
 });
