89b04fed9267be250de4b5484737551a5c170936
Alain Horner <alain.horner@liip.ch>
2012-06-23 18:42:29 +0200

    introduced app framework for handling the different sites

--- js/main.js
+++ js/main.js
@@ -2,59 +2,33 @@ YUI({
     modules: {
         'combiner': {
             fullpath: "js/combiner.js"
+        },
+        'home-view': {
+            fullpath: "js/view/HomeView.js"
         }
     }
-}).use('node', 'combiner', function (Y) {
+}).use('node', 'app-base', 'app-transitions', 'home-view', function (Y) {
 
     'use strict';
 
-    window.onload = function() {
-        var encryptedCanvas = Y.one('#encrypted canvas'),
-            containerContext = Y.one('#container canvas').invoke('getContext', '2d'),
-            originalContext = Y.one('#original canvas').invoke('getContext', '2d'),
-            hiddenContext = Y.one('#hidden canvas').invoke('getContext', '2d'),
-            encryptedContext = encryptedCanvas.invoke('getContext', '2d'),
-            decryptedContext = Y.one('#decrypted canvas').invoke('getContext', '2d'),
-            containerImg = new Image(),
-            originalImg = new Image(),
-            combiner = new Y.Combiner();
+    var app;
 
-        // draw original image
-        containerImg.onload = function() {
-          containerContext.drawImage(containerImg, 0, 0, 300, 300);
-        };
+    app = new Y.App({
+        transitions: true,
+        viewContainer: '#content',
+        views: {
+            home: {
+                type: 'HomeView'
+            }
+        },
+        routes: [{
+            path: '/',
+            callback: function () {
+                this.showView('home');
+            }
+        }]
+    });
 
-        originalImg.onload = function() {
-          originalContext.drawImage(originalImg, 0, 0, 300, 300);
-        };
-
-        containerImg.src = "img/lena.png";
-        originalImg.src = "img/fribourg.png";
-
-        // minify original image
-        setTimeout(function () {
-            var minifiedImageData = combiner.minify(originalContext.getImageData(0, 0, 300, 300), 255);// TODO: could we directly pass it the image?
-            hiddenContext.putImageData(minifiedImageData, 0, 0);
-        }, 200);// TODO: improve, so that we somehow get an event
-
-        // hide original image in container image
-        setTimeout(function () {
-            var combinedImageData = combiner.combine(containerContext.getImageData(0, 0, 300, 300), originalContext.getImageData(0, 0, 300, 300));
-            encryptedContext.putImageData(combinedImageData, 0, 0);
-        }, 200);// TODO: improve, so that we somehow get an event
-
-        // extract hidden image from container image
-        setTimeout(function () {
-            var extractedImageData = combiner.extract(encryptedContext.getImageData(0, 0, 300, 300));
-            decryptedContext.putImageData(extractedImageData, 0, 0);
-        }, 500);// TODO: improve, so that we somehow get an event
-
-        // write extracted image to the DOM as an actual PNG
-        setTimeout(function () {
-            var imgData = encryptedCanvas.invoke('toDataURL', 'image/png');
-            Y.one('#result').append('<img src="' + imgData + '" class="thumbnail"/>');
-        }, 800);// TODO: improve, so that we somehow get an event
-
-    };
+    app.navigate('/');
 
 });
---
+++ js/view/HomeView.js
@@ -0,0 +1,70 @@
+YUI.add('home-view', function (Y) {
+
+    'use strict';
+
+    Y.HomeView = Y.Base.create('homeView', Y.View, [], {
+
+        generateImages: function () {
+            var encryptedCanvas = this.get('container').one('#encrypted canvas'),
+                containerContext = this.get('container').one('#container canvas').invoke('getContext', '2d'),
+                originalContext = this.get('container').one('#original canvas').invoke('getContext', '2d'),
+                hiddenContext = this.get('container').one('#hidden canvas').invoke('getContext', '2d'),
+                encryptedContext = encryptedCanvas.invoke('getContext', '2d'),
+                decryptedContext = this.get('container').one('#decrypted canvas').invoke('getContext', '2d'),
+                containerImg = new Image(),
+                originalImg = new Image(),
+                combiner = new Y.Combiner();
+
+            // draw original image
+            containerImg.onload = function() {
+              containerContext.drawImage(containerImg, 0, 0, 300, 300);
+            };
+
+            originalImg.onload = function() {
+              originalContext.drawImage(originalImg, 0, 0, 300, 300);
+            };
+
+            containerImg.src = "img/lena.png";
+            originalImg.src = "img/fribourg.png";
+
+            // minify original image
+            setTimeout(function () {
+                var minifiedImageData = combiner.minify(originalContext.getImageData(0, 0, 300, 300), 255);// TODO: could we directly pass it the image?
+                hiddenContext.putImageData(minifiedImageData, 0, 0);
+            }, 200);// TODO: improve, so that we somehow get an event
+
+            // hide original image in container image
+            setTimeout(function () {
+                var combinedImageData = combiner.combine(containerContext.getImageData(0, 0, 300, 300), originalContext.getImageData(0, 0, 300, 300));
+                encryptedContext.putImageData(combinedImageData, 0, 0);
+            }, 200);// TODO: improve, so that we somehow get an event
+
+            // extract hidden image from container image
+            setTimeout(function () {
+                var extractedImageData = combiner.extract(encryptedContext.getImageData(0, 0, 300, 300));
+                decryptedContext.putImageData(extractedImageData, 0, 0);
+            }, 500);// TODO: improve, so that we somehow get an event
+
+            // write extracted image to the DOM as an actual PNG
+            setTimeout(function () {
+                var imgData = encryptedCanvas.invoke('toDataURL', 'image/png');
+                Y.one('#result').append('<img src="' + imgData + '" class="thumbnail"/>');
+            }, 800);// TODO: improve, so that we somehow get an event*/
+
+        },
+
+        render: function () {
+            var html = Y.one('#template_home').getContent(),
+                inst = this;
+
+            this.get('container').setHTML(html);
+
+            setTimeout(function () {
+                inst.generateImages();
+            }, 100)// TODO: is there no event or callback for set html?
+
+        }
+
+    });
+
+}, '0.1', {requires: ['view', 'combiner']});
