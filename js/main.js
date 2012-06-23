b7c3a24f74abcc2fc57fe80e45a83fef08aa5202
Alain Horner <alain.horner@liip.ch>
2012-06-23 17:55:14 +0200

    render downloadable encrypted image

--- js/main.js
+++ js/main.js
@@ -9,10 +9,11 @@ YUI({
     'use strict';
 
     window.onload = function() {
-        var containerContext = Y.one('#container canvas').invoke('getContext', '2d'),
+        var encryptedCanvas = Y.one('#encrypted canvas'),
+            containerContext = Y.one('#container canvas').invoke('getContext', '2d'),
             originalContext = Y.one('#original canvas').invoke('getContext', '2d'),
             hiddenContext = Y.one('#hidden canvas').invoke('getContext', '2d'),
-            encryptedContext = Y.one('#encrypted canvas').invoke('getContext', '2d'),
+            encryptedContext = encryptedCanvas.invoke('getContext', '2d'),
             decryptedContext = Y.one('#decrypted canvas').invoke('getContext', '2d'),
             containerImg = new Image(),
             originalImg = new Image(),
@@ -40,13 +41,19 @@ YUI({
         setTimeout(function () {
             var combinedImageData = combiner.combine(containerContext.getImageData(0, 0, 300, 300), originalContext.getImageData(0, 0, 300, 300));
             encryptedContext.putImageData(combinedImageData, 0, 0);
-        }, 100);// TODO: improve, so that we somehow get an event
+        }, 200);// TODO: improve, so that we somehow get an event
 
         // extract hidden image from container image
         setTimeout(function () {
             var extractedImageData = combiner.extract(encryptedContext.getImageData(0, 0, 300, 300));
             decryptedContext.putImageData(extractedImageData, 0, 0);
-        }, 1000);// TODO: improve, so that we somehow get an event
+        }, 500);// TODO: improve, so that we somehow get an event
+
+        // write extracted image to the DOM as an actual PNG
+        setTimeout(function () {
+            var imgData = encryptedCanvas.invoke('toDataURL', 'image/png');
+            Y.one('#result').append('<img src="' + imgData + '"/>');
+        }, 800);// TODO: improve, so that we somehow get an event
 
     };
 
