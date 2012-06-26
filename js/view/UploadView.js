1337ea679bb8a1cf66f876ce831f37536e2a0964
Alain Horner <alain.horner@liip.ch>
2012-06-26 13:30:57 +0200

    Fixed dropbox in firefox

--- js/view/UploadView.js
+++ js/view/UploadView.js
@@ -10,7 +10,6 @@ YUI.add('upload-view', function (Y) {
                 reader,
                 inst = this;
 
-            e.stopPropagation();
             e.preventDefault();
 
             // return if we didn't get a file
@@ -30,6 +29,10 @@ YUI.add('upload-view', function (Y) {
             reader.readAsDataURL(file);
         },
 
+        _handleUnneededEvent: function (e) {
+            e.preventDefault();
+        },
+
         processUploadedImage: function (src) {
             this.get('container').append('<img src="' + src + '" class="thumbnail"/>');
             // TODO: hide it in lena here
@@ -37,7 +40,8 @@ YUI.add('upload-view', function (Y) {
 
         initUI: function () {
             var dropbox = this.get('container').one('#dropbox');
-            dropbox.on('drop', this._handleFileDrop, this);// TODO: make this work in firefox
+            dropbox.on('dragover', this._handleUnneededEvent, this);
+            dropbox.on('drop', this._handleFileDrop, this);
         },
 
         render: function () {
